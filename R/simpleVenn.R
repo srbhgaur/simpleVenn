f1cace7979fdbe40ba4026e660ee39e5d7a20bbc
Saurabh Gaur <sr7@saurabhgaur.com>
2015-07-27 19:50:29 +0530

    Hardcoded background. No labels

--- R/simpleVenn.R
+++ R/simpleVenn.R
@@ -1,15 +1,38 @@
-#' <Add Title>
+#' Create Simple Venn Diagrams
 #'
-#' <Add Description>
+#' Create Simple Venn Diagrams
 #'
 #' @import htmlwidgets
 #'
 #' @export
-simpleVenn <- function(jsondata, width = NULL, height = NULL) {
+simpleVenn <- function(sets,labels, width = NULL, height = NULL) {
+
+  count = length(sets)
+  if(count==2){
+    AB=length(intersect(sets[[1]],sets[[2]]))
+    A=length(sets[[1]])-AB
+    B=length(sets[[2]])-AB
+    setdata = jsonlite::toJSON(data.frame(set=c("A","B","AB"),size=c(A,B,AB)))
+  }
+  if(count==3) {
+    ABC=length(intersect(sets[[1]],intersect(sets[[2]],sets[3])))
+    AB=length(intersect(sets[[1]],sets[[2]]))-ABC
+    AC=length(intersect(sets[[1]],sets[[3]]))-ABC
+    BC=length(intersect(sets[[2]],sets[[3]]))-ABC
+    A=length(sets[[1]])-ABC-AC-AB
+    B=length(sets[[2]])-ABC-AB-BC
+    C=length(sets[[3]])-ABC-AC-BC
+    setdata = jsonlite::toJSON(data.frame(set=c("A","B","C","AB","AC","BC","ABC"),size=c(A,B,C,AB,AC,BC,ABC)))
+  }
+  if(count!=2&count!=3){ print("Invalid Count") }
+
+  #if labels not present,use names(list) or ABC
 
   # forward options using x
   x = list(
-    jsondata = jsondata
+    setdata = setdata,
+    count = count,
+    labels = labels
   )
 
   # create widget
@@ -46,3 +69,6 @@ simpleVenn_html <- function(id, style, class, ...){
     )
   )
 }
+
+
+
--- inst/htmlwidgets/simpleVenn.js
+++ inst/htmlwidgets/simpleVenn.js
@@ -13,12 +13,37 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
+      var vis = d3.select("#venn").append("svg");
+
       //SimpleVenn Fixed numbers 2/3 Fixed Area
       if(x.count==2){
         //Make two circles and label them
+        var circledata = [{"cx":75,"cy":75,"r":50,"color":"red"},
+                          {"cx":150,"cy":75,"r":50,"color":"blue"}]
+        var circles = vis.selectAll("circle")
+            .data(circledata)
+            .enter()
+            .append("circle")
+            .attr("opacity", 0.5)
+            .attr("fill", function(d){return d.color;})
+            .attr("cx", function(d){return d.cx;})
+            .attr("cy", function(d){return d.cy;})
+            .attr("r", function(d){return d.r;})
 
       } else if(x.count==3){
         //Make two circles and label them
+         var circledata = [{"cx":50,"cy":75,"r":50,"color":"red"},
+                          {"cx":126,"cy":75,"r":50,"color":"blue"},
+                          {"cx":88,"cy":138,"r":50,"color":"green"}]
+        var circles = vis.selectAll("circle")
+            .data(circledata)
+            .enter()
+            .append("circle")
+            .attr("opacity", 0.5)
+            .attr("fill", function(d){return d.color;})
+            .attr("cx", function(d){return d.cx;})
+            .attr("cy", function(d){return d.cy;})
+            .attr("r", function(d){return d.r;})
 
       }
 
